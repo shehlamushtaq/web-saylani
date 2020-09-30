c05b373a64bc7804f03d58a8cf8e9eee779397c2
unknown <shehla.asim.78@gmail.com>
2020-09-30 11:05:13 +0500

    teacher given task

---
+++ 30sep2020/index.js
@@ -0,0 +1,15 @@
+
+var age=16;
+
+var per='no';
+
+var cnic='have it'
+
+if ((age >= 15 && per=="yes") || (cnic=="have it") ){
+    console.log('yu have entered')
+
+}
+else
+{
+    console.log("you are not allowed")
+}
