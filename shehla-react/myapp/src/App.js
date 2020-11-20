c5c0880e0042ecf62d4008a9079c62540d477cff
shehlamushtaq <shehla.asim.78@gmail.com>
2020-11-20 12:36:41 +0500

    20/11/2020 on Fiday

---
+++ shehla-react/myapp/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './Header'
+import Footer from './Footer';
+import Link from './LInk';
+
+
+function App() {
+  return (
+  <div>
+    <h1>My First App</h1>
+       <Header/>     
+      <Link/>
+      <Footer/>
+
+  </div>
+  );
+}
+
+export default App;
---
+++ shehla-react/myapp/src/Header.js
@@ -0,0 +1,24 @@
+import mypic from './images/p1.jpg'
+import mypic2 from './images/f2.jpg'
+import Links from './LInk'
+function Header () {
+    return (
+        <>
+        <h1 >Shehla Mushtaq </h1>
+        
+        <img src={mypic} width="200px"></img>
+        <img src={mypic2} width="200px" height="200px"></img>
+        <img src={mypic} width="200px"></img>
+        <img src={mypic2} width="200px" height="200px"></img>
+        <img src={mypic} width="200px"></img>
+        <img src={mypic2} width="200px" height="200px"></img><br>git</br>
+        <Links/>
+        
+        
+        <h3>Student of Web Graphics</h3>
+        <h5>Learning from Teacher Faiza</h5>
+        
+        </>
+    );
+}
+export default Header;
